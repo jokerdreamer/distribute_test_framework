7d83b61f6c824a2b20236e52cf4840c16195e7a0
miaolong <joker_dreamer@126.com>
2017-07-06 15:38:52 +0800

    Update test.sh

--- cache_test/sh/test.sh
+++ cache_test/sh/test.sh
@@ -1,18 +1,17 @@
 #!/bin/bash
 
-JAVA_HOME=$JAVA_HOME/bin/java
 
 if [ $1 == "-client" ] ; then
-    $JAVA_HOME cache_test.Client $2 $3 $4 $5 $6 $7 $8 $9
+    java -cp zookeeper-3.4.6.jar:cache_test.jar:slf4j-log4j12-1.7.7.jar:slf4j-api-1.7.5.jar:log4j-1.2.15.jar cache_test.Client $2 $3 $4 $5 $6 $7 $8 $9 $10
 elif [ $1 == "-monitor" ] ; then
-	$JAVA_HOME cache_test.Master $2
+    java -cp zookeeper-3.4.6.jar:cache_test.jar:slf4j-log4j12-1.7.7.jar:slf4j-api-1.7.5.jar:log4j-1.2.15.jar cache_test.Master $2
 elif [ $1 == "-worker" ] ; then
-	$JAVA_HOME cache_test cache_test.Worker $2 $3
+    java -cp zookeeper-3.4.6.jar:cache_test.jar:slf4j-log4j12-1.7.7.jar:slf4j-api-1.7.5.jar:log4j-1.2.15.jar cache_test.Worker $2 $3
 elif [ $1 == "-help" ] ; then
-    echo "-client zookeeper task duration url concurrency thread workerList"
+    echo "-client zookeeper task duration url concurrency thread workerList script"
     echo "-monitor zookeeper"
     echo "-worker zookeeper dir"
     echo "-help"
 else
-	echo "use -help as option for detail"
+    echo "use -help as option for detail"
 fi
